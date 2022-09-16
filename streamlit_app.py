ec2d11ba566ba618baadced7331f2108e2e274d2
TaylorRetsu <111224332+TaylorRetsu@users.noreply.github.com>
2022-09-16 22:47:56 +0100

    Update streamlit_app.py
    Added error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,16 +31,19 @@ streamlit.write('The user entered ', fruit_choice)
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      steamlit.error("Please select a fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # take the json version of the response and normalise it
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())      
+      # output it to the screen as a table
+      streamlit.dataframe(fruityvice_normalized)
 
-# take the json version of the response and normalise it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
-# don't run anything past here while we troubleshoot
-streamlit.stop()
+except URLError as e:
+  streamlit.stop()
 
 #import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
