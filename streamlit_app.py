bf74e2b63dd9950bebfb2a6f8926d2d69b73170b
TaylorRetsu <111224332+TaylorRetsu@users.noreply.github.com>
2022-09-16 22:50:16 +0100

    Update streamlit_app.py
    corrected indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    steamlit.error("Please select a fruit to get information.")
-    else:
+       steamlit.error("Please select a fruit to get information.")
+  else:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       # take the json version of the response and normalise it
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())      
